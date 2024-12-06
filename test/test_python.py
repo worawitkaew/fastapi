48354e0f8fbfa33fb8dab1a94aa50e02dfa6974c
diamond <mon23503516@gmail.com>
2024-12-06 16:53:54 +0700

    add login test

--- test/test_python.py
+++ test/test_python.py
@@ -19,3 +19,24 @@ def test_ProductType1(test_client):
     assert response.json() == {
     "message_test": "Hello World_test"
     }
+
+@pytest.fixture
+def get_token(test_client):
+    # ส่ง request สำหรับ login และดึง token
+    login_data = {"username": "username_test", "password": "password_test"}
+    response = test_client.post("/routes/pre_route/token", data=login_data)
+    assert response.status_code == 200
+    return response.json()["access_token"]  # หรือใช้ข้อมูลที่ส่งกลับจาก API ตามที่ต้องการ
+
+# ทดสอบ API ที่ต้องการ authentication
+@pytest.mark.asyncio
+def test_login(test_client, get_token):
+    headers = {"Authorization": f"Bearer {get_token}"}
+    response = test_client.get("/", headers=headers)
+    assert response.json() == {
+  "message": "Hello World",
+  "current_user": {
+    "username": "username_test",
+    "disabled": False
+  }
+}
